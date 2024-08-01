a57a90f6d06c0dbbcd2e7ef6dfc8d2f2ba1036b1
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-08-01 11:30:43 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -44,9 +45,9 @@ export const setupServer = () => {
     }
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Contact not found',
+  app.use((req, res, next) => {
+    res.status(404).send({
+      message: 'Not found',
     });
   });
 

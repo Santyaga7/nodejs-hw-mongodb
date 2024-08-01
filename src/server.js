5ce2b98385e9d11ffa08f21510a193572cd6ebf0
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-08-01 12:39:01 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -29,20 +29,25 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
+  app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
     if (!contact) {
       res.status(404).json({
-        status: 404,
         message: 'Contact not found',
       });
       return;
     }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
 
-  app.use((req, res, next) => {
-    res.status(404).send({
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
       message: 'Not found',
     });
   });

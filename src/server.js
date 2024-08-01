bf43d038c8ffb3f42d48ad859704e178cbcba367
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-08-01 11:50:26 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ export const setupServer = () => {
 
     res.status(200).json({
       status: 200,
-      message: 'Successfully found contactssss!',
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });

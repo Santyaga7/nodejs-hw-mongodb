25c3e7dae27ab0313308086aaf03615faf3213ff
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-08-07 12:32:55 +0300

    Update contacts subsequence.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -11,8 +11,8 @@ export const getAllContactsController = async (req, res) => {
   const contacts = await getAllContacts();
   res.status(200).json({
     status: 200,
-    data: contacts,
     message: 'Successfully found contacts!',
+    data: contacts,
   });
 };
 

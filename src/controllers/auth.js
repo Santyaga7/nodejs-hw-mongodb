7ce38b414a59e812eed002a8f4f9da09eae0b41e
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-08-31 12:28:48 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -77,10 +77,10 @@ export const logoutUserController = async (req, res) => {
   res.status(204).send();
 };
 
-export const sendResetEmailController = async (req, res) => {
+export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
+    message: 'Reset password email has been successfully sent.',
     status: 200,
     data: {},
   });
